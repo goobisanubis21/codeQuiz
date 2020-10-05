67461da4086e228174d4c151415b58ab86d2f6d7
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-04 21:52:39 -0400

    created new button function and added more questions and styling

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ var questions = [
         choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
         answer: "Parentheses",
     },
+    {
+        question:
+            "Arrays in JavaScript can be used to store ____.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above",
+    },
+    {
+        question:
+            "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Quotes",
+    },
+    {
+        question:
+            "A very useful tool during development and debugging for printing content to the debugger is ____.",
+        choices: ["Javascript", "Terminal/Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
+    }
 ];
 
 //variables to call upon html ids
@@ -17,6 +35,58 @@ var questionEl = document.querySelector("#question");
 var optionListEl = document.querySelector("#option-list");
 var questionResultEl = document.querySelector("#question-result");
 var timerEl = document.querySelector("#timer");
+
+var highscoreTab = document.createElement("h3");
+highscoreTab.setAttribute("id", "highscore-tab");
+highscoreTab.textContent = "Highscores";
+document.body.appendChild(highscoreTab);
+
+//creating a highscore button
+highscoreTab.addEventListener("click", highscorePage);
+
+//function to render highscore page when clicking on highscore buttion
+function highscorePage() {
+    // clears body of webpage
+    document.body.textContent = "";
+    var highscoreTitle = document.createElement("h1");
+    highscoreTitle.setAttribute("id", "highscore-title");
+    highscoreTitle.textContent = "Highscores:";
+    document.body.appendChild(highscoreTitle);
+    //setting variable of local storage's scores
+    var high_scores = localStorage.getItem("scores");
+    //returns highscore string as an object
+    high_scores = JSON.parse(high_scores);
+    //sorts all names and their scores in decsending order
+    high_scores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    //creating an ul in the dom and setting it to the variable contentUL
+    var contentUL = document.createElement("ul");
+    contentUL.setAttribute("id", "content-ul");
+
+    //loops through the highscore to create the list items of names and scores
+    for (var i = 0; i < high_scores.length; i++) {
+        var contentLI = document.createElement("li");
+        contentLI.textContent =
+            "Name: " + high_scores[i].name + " Score: " + high_scores[i].score;
+        contentUL.appendChild(contentLI);
+    }
+
+    // appends new ul content to the body of the webpage
+    document.body.appendChild(contentUL);
+
+    // creation of clear button
+    var clear = document.createElement("h2");
+    clear.textContent = "Clear Scores";
+    document.body.appendChild(clear);
+
+    // button to clear local storage (highscores)
+    clear.addEventListener("click", function () {
+        localStorage.clear();
+        contentUL.style.display = "none";
+    });
+}
 
 
 //variables to set the question index, 
@@ -33,9 +103,6 @@ function endQuiz() {
     clearInterval(intervalId);
     var body = document.body;
     body.innerHTML = "Game over, You scored " + correctCount;
-    // var gameOver = document.querySelector("#game-over");
-    // window.location.href = ("gameover.html");
-    // gameOver.innerHTML = "Game over, You scored " + correctCount;
     setTimeout(showHighScore, 2);
 }
 
@@ -66,6 +133,7 @@ function showHighScore() {
 
     //creating an ul in the dom and setting it to the variable contentUL
     var contentUL = document.createElement("ul");
+    contentUL.setAttribute("id", "content-ul");
 
     //loops through the highscore to create the list items of names and scores
     for (var i = 0; i < high_scores.length; i++) {
@@ -75,8 +143,29 @@ function showHighScore() {
         contentUL.appendChild(contentLI);
     }
 
-    //appends new ul content to the body of the webpage
+    // appends new ul content to the body of the webpage
     document.body.appendChild(contentUL);
+
+    // creation of clear button
+    var clear = document.createElement("h2");
+    clear.textContent = "Clear Scores";
+    document.body.appendChild(clear);
+
+    // button to clear local storage (highscores)
+    clear.addEventListener("click", function () {
+        localStorage.clear();
+        contentUL.style.display = "none";
+    });
+
+    // creation of play again button
+    var goBack = document.createElement("h3");
+    goBack.setAttribute("id", "play-again")
+    goBack.textContent = "Play Again";
+    document.body.appendChild(goBack);
+    document.addEventListener("click", function () {
+        location.reload()
+    });
+
 }
 
 //function to count down time left in the game
@@ -146,4 +235,4 @@ function checkAnswer(event) {
 //calls the render question function
 renderQuestion();
 //adds and event listener to options listen to store which options is clicked on
-optionListEl.addEventListener("click", checkAnswer);  
+optionListEl.addEventListener("click", checkAnswer);
